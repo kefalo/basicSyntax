043fb468d304f0d1816af39dd92adb1ad30410ac
devvm <stefanmkac@gmail.com>
2020-01-06 01:27:40 +0100

    first python file change
    Signed-off-by: devvm <stefanmkac@gmail.com>

--- BasicSyntax/Loops.py
+++ BasicSyntax/Loops.py
@@ -4,3 +4,8 @@ Conditions are used to stop the execution of loops
 iterable items are Strings, List, Tuple, Dictionary
 """
 
+x = 0
+while x < 10:
+    print("Loop has value of: " + str(x))
+    x = x + 1
+    print("one more line")
