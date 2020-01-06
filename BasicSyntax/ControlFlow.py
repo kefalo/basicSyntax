83a444ac2edaf19a77ed05c0dd72ddfcdf43fab9
devvm <stefanmkac@gmail.com>
2020-01-06 01:21:19 +0100

    first python file change
    Signed-off-by: devvm <stefanmkac@gmail.com>

--- BasicSyntax/ControlFlow.py
+++ BasicSyntax/ControlFlow.py
@@ -1,4 +1,18 @@
 """
-Initialization of first python file
+Conditional Logic
 """
-print(1+2)
+
+if 100 < 10:
+    print("Hundred is greater then 10")
+
+
+value = "orange"
+
+if value == "green":
+    print("Go")
+elif value == "yellow":
+    print("prepare to stop")
+else:
+    print("Stop")
+
+print("It will always gonna print")
---
+++ BasicSyntax/Loops.py
@@ -0,0 +1,6 @@
+"""
+Execute statements repeatedly
+Conditions are used to stop the execution of loops
+iterable items are Strings, List, Tuple, Dictionary
+"""
+
