a3bb422038116127bcc410239cd6529eac1ba2a1
Stefan Marinković <stefanmkac@gmail.com>
2020-01-24 17:02:55 +0100

    Adding two new classes for learning basics of methods and extended version of it.
    methodsBasics.py
    methodsBasics01.py
    builtInFunctions.py
    
    Signed-off-by: QAninja <stefanmkac@gmail.com>

---
+++ methods/builtInFunctions.py
@@ -0,0 +1,11 @@
+"""
+Some built-in functions
+max(): It takes any number of arguments and returns the largest one
+
+min(): It takes any number of arguments and returns the smallest one
+
+abs(): It returns the obsolete value of the number, that number's distance from 0.
+It always returns a positive value and it only takes a single number.
+
+type(): It returns the type of the data it receives as an argument.
+"""
---
+++ methods/methodsBasics.py
@@ -0,0 +1,16 @@
+"""
+Methods group of code statements, which will going to complete some kind of workflow and into one logical entity.
+"""
+
+def sum_nums(n1, n2):
+    print(n1 + n2)
+
+sum_nums(2, 8)
+
+sum_nums(2, 3)
+
+l = [1, 2, 3]
+print(l.append(4))
+print(l)
+
+print(len(l))
