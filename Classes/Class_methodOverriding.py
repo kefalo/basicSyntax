aa6a8c295164b10db2afd82eea539947ff1661a4
devvm <stefanmkac@gmail.com>
2020-05-04 21:33:30 +0200

    git commit - Class Excercise and end of Classes

--- Classes/Class_methodOverriding.py
+++ Classes/Class_methodOverriding.py
@@ -1,3 +1,36 @@
 """
 Method Overriding
 """
+class Car(object):
+    def __init__(self):
+        print("You just created the car instance")
+
+    def drive(self):
+        print("Car started...")
+
+    def stop(self):
+        print("Car stopped")
+
+
+class BMW(Car):
+    def __init__(self):
+        # Car.__init__(self)
+        # another method of calling the __init__(self) method of the Car class
+        #super().__init__()
+        print("You just created the BMW instance")
+
+    def drive(self):
+        super().drive()
+        print("You are driving the BMW, Enjoy...")
+        
+    def headsup_display(self):
+        print("This is unique feature")
+
+#c = Car()
+#c.drive()
+#c.stop()
+
+b = BMW()
+b.drive()
+b.stop()
+b.headsup_display()
